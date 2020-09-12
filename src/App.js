d93a331140c3ba7a706698bb055e4bee97c2b88f
jose <sanchezzz2391@gmail.com>
2020-09-12 12:15:21 -0300

    configuracion de rutas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./views/home/Home";
+import FormCreate from "./components/posts/FormCreate";
+import FormEdit from "./components/posts/FormEdit";
+import PostDetail from "./views/postDetail/PostDetail";
+import NoMatch from "./views/noMatch/NoMatch";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/post/new" component={FormCreate} />
+        <Route path="/post/:id/edit" exact component={FormEdit} />
+        <Route path="/post/detail/:id" exact component={PostDetail} />
+        <Route component={NoMatch} />
+      </Switch>
+    </Router>
   );
 }
 
