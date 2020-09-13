656d3a13ae5b3003ce39e6f6ce3e70ffd8956d5c
jose <sanchezzz2391@gmail.com>
2020-09-12 21:07:25 -0300

    display de data del servicio graphql

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,4 +1,7 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import { Link } from "react-router-dom";
+import { css } from "@emotion/core";
+import Loading from "../loading/Loading";
 import Table from "../table/Table";
 import Thead from "../table/Thead";
 import Tr from "../table/Tr";
@@ -6,51 +9,85 @@ import Th from "../table/Th";
 import Tbody from "../table/Tbody";
 import Td from "../table/Td";
 import Button from "../button/Button";
+import { getAll } from "../../services/post.services";
 
+const override = css`
+  size: 15;
+`;
 export default class List extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      list: [
-        { id: 1, name: "JUAN" },
-        { id: 2, name: "Maria" },
-        { id: 3, name: "JOSE" },
-      ],
+      list: [getAll()],
+      loading: true,
     };
+  }
+  componentDidMount() {
+    const getList = async () => {
+      const list = await getAll();
+      this.setState({ list, loading: false });
+    };
+    getList();
   }
 
   render() {
     return (
-      <div className="App">
-        <Table>
-          <Thead>
-            <Tr>
-              <Th>id</Th>
-              <Th>Name</Th>
-              <Th></Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {this.state.list.map((user, i) => (
-              <Tr key={i}>
-                <Td> {user.id}</Td>
-                <Td> {user.name}</Td>
+      <Fragment>
+        {this.state.loading ? (
+          <Loading size={60} color={"#123abc"} loading={this.state.loading} />
+        ) : (
+          <div className="center">
+            <div className="left">
+              <Link to="/post/new">
+                <button className="btn button">Crear Post</button>
+              </Link>
+            </div>
+            <Table>
+              <Thead>
+                <Tr>
+                  <Th>#</Th>
+                  <Th>Titulo</Th>
+                  <Th>Contenido</Th>
+                  <Th>Autor</Th>
+                  <Th>Acciones</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {this.state.list.length > 0
+                  ? this.state.list.map((post, i) => (
+                      <Tr key={i}>
+                        <Td> {post.id}</Td>
+                        <Td> {post.title}</Td>
+                        <Td> {post.body}</Td>
+                        <Td> {post.user && post.user.name}</Td>
+                        <Td>
+                          <div className="center">
+                            <Link to={`/post/detail/${post.id}`}>
+                              <Button styles={"btn button"}>Detalle</Button>
+                            </Link>
 
-                <Td>
-                  <Button style={"primary"} event={() => console.log("edit")}>
-                    RING
-                  </Button>
-                </Td>
-                <Td>
-                  <Button style={"danger"} event={() => console.log("delete")}>
-                    KILL
-                  </Button>
-                </Td>
-              </Tr>
-            ))}
-          </Tbody>
-        </Table>
-      </div>
+                            <Link to="/">
+                              <Button styles={"btn button-yellow"}>
+                                Editar
+                              </Button>
+                            </Link>
+
+                            <Button
+                              styles={"btn button-red"}
+                              event={() => console.log("delete")}
+                            >
+                              Eliminar
+                            </Button>
+                          </div>
+                        </Td>
+                      </Tr>
+                    ))
+                  : null}
+              </Tbody>
+            </Table>
+          </div>
+        )}
+      </Fragment>
     );
   }
 }
--- src/views/postDetail/PostDetail.jsx
+++ src/views/postDetail/PostDetail.jsx
@@ -1,12 +1,64 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { getPost } from "../../services/post.services";
+import Button from "../../components/button/Button";
+import Loading from "../../components/loading/Loading";
 
 export default class PostDetail extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      post: {},
+      loading: true,
+    };
   }
 
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    const get = async () => {
+      const post = await getPost(id);
+      this.setState({ post, loading: false });
+    };
+    get();
+  }
   render() {
-    return <div>detail post</div>;
+    const { id, title, body, user, comments } = this.state.post;
+    const { loading } = this.state;
+    return (
+      <div className="center">
+        <h1>Detalle del post</h1>
+        {loading ? (
+          <Loading size={60} color={"#123abc"} loading={this.state.loading} />
+        ) : (
+          <div className="center">
+            <ul>
+              <li>{id} </li>
+              <li>{title} </li>
+              <li>{body} </li>
+              <ul>
+                <li>{user.name} </li>
+                <li>{user.email} </li>
+              </ul>
+            </ul>
+            <div>
+              <h2>Comentarios</h2>
+              {comments &&
+                comments.data.map((comment, index) => (
+                  <ul key={index}>
+                    <li>{comment.name}</li>
+                    <li>{comment.email}</li>
+                  </ul>
+                ))}
+            </div>
+
+            <div className="center">
+              <Link to="/">
+                <Button styles={"btn button"}>Volver</Button>
+              </Link>
+            </div>
+          </div>
+        )}
+      </div>
+    );
   }
 }
