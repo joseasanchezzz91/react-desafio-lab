1887ab04b1d8a7ad0ba07711be3564f7dca9be3e
jose <sanchezzz2391@gmail.com>
2020-09-13 21:43:55 -0300

    conectado el create y el edit a los servicios
    todo OK

--- src/components/posts/FormCreate.jsx
+++ src/components/posts/FormCreate.jsx
@@ -1,140 +1,87 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { create } from "../../services/post.services";
+import { AlertInfo } from "../alert/Alert";
+import Loading from "../loading/Loading";
 
 export default class FormCreate extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      loading: false,
+    };
   }
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-    var formData = new FormData();
-
-    formData.append("tarifa", e.target.tarifa.value);
-    formData.append("cliente", e.target.cliente.value);
-    formData.append("e1_tipo", e.target.e1_tipo.value);
-    formData.append("e2_tipo", e.target.e2_tipo.value);
-    formData.append("p1_tipo", e.target.p1_tipo.value);
-    formData.append("p2_tipo", e.target.p2_tipo.value);
-    formData.append("p3_tipo", e.target.p3_tipo.value);
-    formData.append("token", this.props.login.user.token);
-    this.props.create(formData, this.handleAlert);
+  handleSubmit = (event) => {
+    event.preventDefault();
+    let formData = new FormData();
+    formData.append("title", event.target.title.value);
+    formData.append("body", event.target.body.value);
+    this.setState({ loading: true });
+    const createPost = (formData) => {
+      return create(formData);
+    };
+    const isCreate = createPost;
+    if (isCreate) {
+      this.setState({ loading: false });
+      AlertInfo(this.props);
+    }
   };
 
   render() {
     return (
-      <div className="">
-        <h2 className="text-center">Creación de Post</h2>
-        <form
-          id=""
-          action=""
-          onSubmit={this.handleLogin}
-          className="center-input"
-        >
-          <div className="pt-5 text-left">
-            <div className="row">
-              <div className="col-md-12 form-group">
-                <div className="form-group">
-                  <label> Tarifa: </label>
-                  <input
-                    autoComplete="off"
-                    name="tarifa"
-                    placeholder="Tarifa"
-                    required={true}
-                    type="text"
-                    className="input-forms"
-                    style={{ backgroundColor: "#ffffff", left: "61px" }}
-                  />
-                </div>
-                <div className="form-group">
-                  <label>Cliente:</label>
-                  <input
-                    autoComplete="off"
-                    name="cliente"
-                    placeholder="Cliente"
-                    required={true}
-                    type="number"
-                    className="input-forms"
-                    style={{ backgroundColor: "#ffffff", left: "50px" }}
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-md-12 form-group">
-                <div className="form-group">
-                  <label className="titulo">Energía tipo 1:</label>
-                  <input
-                    autoComplete="off"
-                    name="e1_tipo"
-                    placeholder="E1_tipo"
-                    required={true}
-                    step="any"
-                    type="number"
-                    className="input-forms"
-                    style={{ backgroundColor: "#ffffff", left: "7px" }}
-                  />
-                </div>
-                <div className="form-group">
-                  <label className="titulo">Energía tipo 2:</label>
-                  <input
-                    autoComplete="off"
-                    name="e2_tipo"
-                    placeholder="E2_tipo"
-                    required={true}
-                    step="any"
-                    type="number"
-                    className="input-forms"
-                    style={{ backgroundColor: "#ffffff", left: "7px" }}
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-md-12 form-group">
-                <div className="form-group">
-                  <label className="titulo">Potencia tipo 1:</label>
-                  <input
-                    autoComplete="off"
-                    name="p1_tipo"
-                    placeholder="P1_tipo"
-                    required={true}
-                    step="any"
-                    type="number"
-                    className="input-forms"
-                    style={{ backgroundColor: "#ffffff" }}
-                  />
-                </div>
-                <div className="form-group">
-                  <label className="titulo">Potencia tipo 2:</label>
-                  <input
-                    autoComplete="off"
-                    name="p2_tipo"
-                    placeholder="P2_tipo"
-                    required={true}
-                    step="any"
-                    type="number"
-                    className="input-forms"
-                    style={{ backgroundColor: "#ffffff" }}
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="center">
-            <div className="inline-btn center">
+      <div>
+        {this.state.loading ? (
+          <Loading size={60} color={"#123abc"} loading={this.state.loading} />
+        ) : (
+          <div>
+            <h2 className="text-center">Creación de Post</h2>
+            <form
+              onSubmit={this.handleSubmit}
+              className="center-input"
+              id=""
+              action=""
+              method="post"
+            >
               <div>
-                <Link to="/">
-                  <button className="btn button-red">Cancelar</button>
-                </Link>
+                <label> Titulo: </label>
+                <input
+                  autoComplete="off"
+                  name="title"
+                  placeholder="Titulo"
+                  required={true}
+                  type="text"
+                  className="input-forms"
+                />
               </div>
-              <div className="">
-                <button className="btn button">Aceptar</button>
+
+              <div>
+                <label>Contenido:</label>
+                <input
+                  autoComplete="off"
+                  name="body"
+                  placeholder="Contenido"
+                  required={true}
+                  type="text"
+                  className="input-forms"
+                />
               </div>
-            </div>
+
+              <div className="center">
+                <div className="inline-btn center">
+                  <div>
+                    <Link to="/">
+                      <button className="btn button-red">Cancelar</button>
+                    </Link>
+                  </div>
+                  <div>
+                    <button className="btn button">Aceptar</button>
+                  </div>
+                </div>
+              </div>
+            </form>
           </div>
-        </form>
+        )}
       </div>
     );
   }
--- src/components/posts/FormEdit.jsx
+++ src/components/posts/FormEdit.jsx
@@ -1,13 +1,94 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { getPost, update } from "../../services/post.services";
+import { AlertInfo } from "../alert/Alert";
+import Loading from "../loading/Loading";
+/**
+ *
+ * Este componente esta elaborado con hooks
+ */
+export default function FormEdit(props) {
+  const [post, setPost] = useState({});
+  const [loading, setLoading] = useState(false);
 
-export default function FormEdit() {
+  useEffect(() => {
+    setLoading(true);
+    const get = async () => {
+      const { id } = props.match.params;
+      const postById = await getPost(id);
+      setPost(postById);
+      setLoading(false);
+    };
+    get();
+  }, [props]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let formData = new FormData();
+    formData.append("title", event.target.title.value);
+    formData.append("body", event.target.body.value);
+    setLoading(true);
+    const updatePost = async () => {
+      return await update(formData);
+    };
+    const isUpdate = updatePost();
+    if (isUpdate) {
+      setLoading(false);
+      AlertInfo(props);
+    }
+  };
 
   return (
     <div>
-      <h1>Form Edit</h1>
-      <form>
-          input
-      </form>
+      <h2 className="text-center">Editar de Post</h2>
+      {loading ? (
+        <div className="">
+          <Loading size={60} color={"#123abc"} loading={loading} />{" "}
+        </div>
+      ) : (
+        <div>
+          <form onSubmit={handleSubmit} className="center-input">
+            <div>
+              <label> Titulo: </label>
+              <input
+                autoComplete="off"
+                name="title"
+                placeholder="Titulo"
+                required={true}
+                type="text"
+                defaultValue={post && post.title}
+                className="input-forms"
+              />
+            </div>
+
+            <div>
+              <label>Contenido:</label>
+              <input
+                autoComplete="off"
+                name="body"
+                placeholder="Contenido"
+                required={true}
+                type="text"
+                defaultValue={post && post.body}
+                className="input-forms"
+              />
+            </div>
+
+            <div className="center">
+              <div className="inline-btn center">
+                <div>
+                  <Link to="/">
+                    <button className="btn button-red">Cancelar</button>
+                  </Link>
+                </div>
+                <div>
+                  <button className="btn button">Aceptar</button>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
